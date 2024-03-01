dabc507e8019348d2689077326432307ac12fc07
davidmarsmalow <davidrenaldy1@gmail.com>
2024-03-01 13:44:32 +0700

    add export to pdf

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -3,13 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiFormatter;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class ExportController extends Controller
 {
+    public function index() {
+        return view('export.index', []);
+    }
+
+    public function generate(Request $request) {
+        $validatedData = $request->validate([
+            'start_date'    => 'required|date|date_format:Y-m-d',
+            'end_date'      => 'required|date|date_format:Y-m-d|after_or_equal:start_date',
+        ]);
+
+        $url = route('bills.index', [
+            'start_date'    => $validatedData['start_date'],
+            'end_date'      => $validatedData['end_date'],
+        ]);
+        $getBills = Request::create($url, 'GET');
+        $response = Route::dispatch($getBills);
+        $responseContent = $response->getContent();
+
+        $dataBill = json_decode($responseContent, true);
+        
+        foreach ($dataBill['data'] as $key => $bill) {
+            $dataBill['data'][$key]['base64_img'] = 'data:image/png;base64,' . base64_encode(Storage::get($bill['image_path']));
+        }
+
+        $pdf = Pdf::loadView('export.pdf', ['dataBill' => $dataBill['data']]);
+
+        return $pdf->download(Carbon::now() . '.pdf');
+    }
+
     public function pdf(Request $request) {
         $input = $request->all();
 
@@ -41,9 +73,10 @@ class ExportController extends Controller
 
         $dataBill = json_decode($responseContent, true);
         
-        
+        $pdf = Pdf::loadView('pdf', ['dataBill' => $dataBill['data']]);
+        return $pdf->download('test.pdf');
 
-        return $responseContent;
+        // return $responseContent;
     }
 
     public function docx() {
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExportController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pdf');
+});
+
+Route::group(['prefix' => '/export'], function () {
+    Route::get('/', [ExportController::class, 'index']);
+    Route::post('/', [ExportController::class, 'generate']);
+    Route::post('/pdf', [ExportController::class, 'pdf']);
+    Route::post('/docx', [ExportController::class, 'docx']);
 });
